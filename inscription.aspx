a8f1ac5f7970901e159d1bc1c716d64fcbc99af2
ivan <ivanjose_04@hotmail.com>
2024-03-18 23:33:54 -0400

    first commit

---
+++ inscription.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="inscription.aspx.cs" Inherits="prjWebCsFriendBookIvan.inscription" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Inscription</title>
+    <link href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" rel="stylesheet" />
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Username: <asp:TextBox ID="txtUsername" runat="server"></asp:TextBox><br />
+            Password: <asp:TextBox ID="txtPassword" TextMode="Password" runat="server"></asp:TextBox><br />
+            Email: <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox><br />
+            Sex: <asp:DropDownList ID="ddlSex" runat="server">
+                 </asp:DropDownList><br />
+            Birth Date: <asp:TextBox ID="txtBirthDate" runat="server"></asp:TextBox><br />
+            Ethnic Group: <asp:TextBox ID="txtEthnicGroup" runat="server"></asp:TextBox><br />
+            Search Reason: <asp:TextBox ID="txtSearchReason" runat="server"></asp:TextBox><br />
+            <asp:Button ID="btnSubmit" Text="Submit" runat="server" OnClick="btnSubmit_Click" />
+        </div>
+    </form>
+
+    <script type="text/javascript">
+        $(function () {
+            $("#<%= txtBirthDate.ClientID %>").datepicker({
+            changeMonth: true,
+            changeYear: true,
+            yearRange: "-100:+0",
+            dateFormat: "yy-mm-dd"
+        });
+    });
+</script>
+
+</body>
+</html>
---
+++ inscription.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace prjWebCsFriendBookIvan
+{
+    public partial class inscription : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CreateGenders();
+            }
+        }
+
+        private void CreateGenders()
+        {
+            ListItem item = new ListItem();
+
+            item.Text = "Select...";
+            item.Value = "";
+
+            ddlSex.Items.Add(item);
+
+            ddlSex.Items.Add(new ListItem("Male", "M"));
+            ddlSex.Items.Add(new ListItem("Woman", "W"));
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //Connexion DB
+            SqlConnection myconn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\ivanj\\Desktop\\AAA\\teccart\\session-4\\at\\prjWebCsFriendBookIvan\\App_Data\\DBFriendBook.mdf;Integrated Security=True");
+
+            //Open connexion
+            myconn.Open();
+
+            //Sql Query
+            string sql = "INSERT INTO Users (Username, Password, Email, Sex, BirthDate, EthnicGroup, SearchReason, CreationDate, LastModification) VALUES ";
+            sql += "(@Username, @Password, @Email, @Sex, @BirthDate, @EthnicGroup, @SearchReason, @CreationDate, @LastModification)";
+
+            //Create command
+            SqlCommand mycmd = new SqlCommand(sql, myconn);
+
+            //Create parameters
+            mycmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+            mycmd.Parameters.AddWithValue("@Password", txtPassword.Text);
+            mycmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+            mycmd.Parameters.AddWithValue("@Sex", ddlSex.SelectedValue);
+            mycmd.Parameters.AddWithValue("@BirthDate", DateTime.Parse(txtBirthDate.Text));
+            mycmd.Parameters.AddWithValue("@EthnicGroup", txtEthnicGroup.Text);
+            mycmd.Parameters.AddWithValue("@SearchReason", txtSearchReason.Text);
+            mycmd.Parameters.AddWithValue("@CreationDate", DateTime.Now);
+            mycmd.Parameters.AddWithValue("@LastModification", DateTime.Now);
+
+            //Execute Sql Query
+            mycmd.ExecuteNonQuery();
+
+            //Close connexion
+            myconn.Close();
+
+            Response.Write("todo bien");
+
+            ClearFormFields();
+
+        }
+
+        protected void ClearFormFields()
+        {
+            txtUsername.Text = string.Empty;
+            txtPassword.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+            ddlSex.ClearSelection();
+            txtBirthDate.Text = string.Empty;
+            txtEthnicGroup.Text = string.Empty;
+            txtSearchReason.Text = string.Empty;
+        }
+
+    }
+}
